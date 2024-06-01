8b01ec8a339370443bc091f05f4dd8322aa2d292
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2024-06-01 14:09:55 +0200

    auth pages UI is don

--- my-app/app/(auth)/sign-in.jsx
+++ my-app/app/(auth)/sign-in.jsx
@@ -1,10 +1,11 @@
 import { View, Text, ScrollView, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { useState } from 'react'
+import { useState } from 'react';
+import { Link } from 'expo-router';
 
 import { images } from "../../constants"
 import FormField from '../../components/FormField';
-import  CustomButton from '../../components/FormField';
+import  CustomButton from '../../components/CustomButton';
 
 const SignIn = () => {
   const [ form, setForm ] = useState({ 
@@ -22,7 +23,7 @@ const SignIn = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-content h-full px-4 my-6">
+        <View className="w-full justify-content h-full px-4 my-6 mt-20">
            <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]" />
            <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
 
@@ -38,12 +39,24 @@ const SignIn = () => {
              title="Password"
              value={form.password}
              handleChangeText={(e) => setForm({...form, password: e})}
-             otherStyles="mt-7"
+             otherStyles="mt-7" 
            />
 
            <CustomButton title="Sign In"
             handlePres={submit}
-            containerStyles='mt-7' />
+            containerStyles='mt-7'
+            isLoading={isSubmitting} />
+
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary">
+              Signup
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -51,3 +64,4 @@ const SignIn = () => {
 }
 
 export default SignIn
+
