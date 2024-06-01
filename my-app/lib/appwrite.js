4dfd713b24bc313eaed828549d956334386a42d1
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2024-06-01 18:01:21 +0200

    auth backend in done

--- my-app/lib/appwrite.js
+++ my-app/lib/appwrite.js
@@ -1,4 +1,5 @@
-export const appwriteConfig = {
+import { Account, Client, ID } from 'react-native-appwrite';
+export const config = {
     endpoint: 'https://cloud.appwrite.io/v1',
     platform: 'com.jsm.aora',
     projectId: "665b006200078cc602c5",
@@ -7,3 +8,26 @@ export const appwriteConfig = {
     videoCollectionId: "665b04fc000aa1ac1262",
     storageId: "665b088d0037212a50b5",
 }
+
+
+// Init your React Native SDK
+const client = new Client();
+
+client
+    .setEndpoint(config.endpoint) // Your Appwrite Endpoint
+    .setProject(config.projectId) // Your project ID
+    .setPlatform(config.platform) // Your application ID or bundle ID.
+
+    const account = new Account(client);
+
+    export const createUser = () => {
+        account.create(ID.unique(), 'me@example.com', 'password', 'Jane Doe')
+        .then(function (response) {
+            console.log(response);
+        }, function (error) {
+            console.log(error);
+        });
+    
+    }
+    
+
